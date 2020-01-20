dedc9c46ebf081c91a3033696141b6b7c458b4a5
Roman Lunev <Roman.Lunev@noveogroup.com>
2020-01-20 21:12:09 +0300

    [Front] Add state and props

--- src/components/Weather/CurrentDate/index.jsx
+++ src/components/Weather/CurrentDate/index.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
-function CurrentDate() {
-  return (
-    <div className="Current-date">
-      <div className="Current-date-main">
-        <div className="Current-date-degree">10&#176;</div>
-        <div className="Current-date-type"></div>
+class CurrentDate extends Component {
+  render() {
+    return (
+      <div className="Current-date">
+        <div className="Current-date-main">
+          <div className="Current-date-degree">{ this.props.temp || 'X' }&#176;</div>
+          <div className="Current-date-type">
+            <img
+              src={ this.props.img ? 'https://www.metaweather.com/static/img/weather/' + this.props.img + '.svg' : '' }
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="Current-date-info">
+          <span>Min { this.props.minTemp || 'X' }&#176;</span>
+          <span>Max { this.props.maxTemp || 'X' }&#176;</span>
+          <span>Wind { this.props.wind || 'X' } м/с { this.props.windDirection || '' }</span>
+        </div>
       </div>
-      <div className="Current-date-info">
-        <span>Min 1&#176;</span>
-        <span>Max 5&#176;</span>
-        <span>Wind 2м/с &#8593;</span>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CurrentDate;
